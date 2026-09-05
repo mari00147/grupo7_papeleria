27363a65b29c1c2ae6edbebe43c20e08e0088009
Sebastian <s.sarrazola@visiontravelmail.net>
2026-09-05 14:50:22 -0500

    crea todas las clases

--- src/model.domain/Producto.java
+++ src/model.domain/Producto.java
@@ -1,46 +1,43 @@
 public abstract class Producto {
 
-private String codigo;
+    private String codigo;
 
-private String nombre;
+    private String nombre;
 
-private double precio;
+    private double precio;
 
-private int cantidadStock;
+    private int cantidadStock;
 
+    public String getCodigo() {
+        return codigo;
+    }
 
-public String getCodigo() {
-    return codigo;
-}
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 
-public void setCodigo(String codigo) {
-    this.codigo = codigo;
-}
+    public String getNombre() {
+        return nombre;
+    }
 
-public String getNombre() {
-    return nombre;
-}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-public void setNombre(String nombre) {
-    this.nombre = nombre;
-}
+    public double getPrecio() {
+        return precio;
+    }
 
-public double getPrecio() {
-    return precio;
-}
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 
-public void setPrecio(double precio) {
-    this.precio = precio;
-}
+    public int getCantidadStock() {
+        return cantidadStock;
+    }
 
-public int getCantidadStock() {
-    return cantidadStock;
-}
-
-public void setCantidadStock(int cantidadStock) {
-    this.cantidadStock = cantidadStock;
-}
-
-
+    public void setCantidadStock(int cantidadStock) {
+        this.cantidadStock = cantidadStock;
+    }
 
 }
