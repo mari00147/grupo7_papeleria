8c00201a152128ead1a6508a24025dfdf6238ad6
Aska <laura2529martinez@gmail.com>
2026-09-06 18:36:59 -0500

    :wip: Avance proyecto Laura

--- src/model.domain/ProductoEscolar.java
+++ src/model.domain/ProductoEscolar.java
@@ -1,3 +1,25 @@
-public class ProductoEscolar {
+public class ProductoEscolar extends Producto {
+    private String nivelEscolar;
 
+    public ProductoEscolar(String nivelEscolar) {
+    super(null, null, 0.0, 0);
+    this.nivelEscolar = nivelEscolar;
+    }
+
+    public String getNivelEscolar() {
+        return nivelEscolar;
+    }
+
+    public void setNivelEscolar(String nivelEscolar) {
+        this.nivelEscolar = nivelEscolar;
+    }
+    
+    @Override
+    public double calcularPrecioFinal() {
+        if (this.nivelEscolar != null && this.nivelEscolar.equalsIgnoreCase("primaria")) {
+            return getPrecio() * 0.90;
+        }
+        return getPrecio();
+    }
 }
+
