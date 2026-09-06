5ad058666c0898dda0454c037bc31f0de13bfffc
mari00147 <mj543895@gmail.com>
2026-09-06 17:38:01 -0500

    crea la clase Proveedor, constructor, Getters - Setters

--- src/model.domain/Proveedor.java
+++ src/model.domain/Proveedor.java
@@ -1,3 +1,39 @@
+
 public class Proveedor {
+
+    private String nombre;
+    private String telefono;
+    private String categoriaProductos;
+
+    public Proveedor(String categoriaProductos, String nombre, String telefono) {
+        this.categoriaProductos = categoriaProductos;
+        this.nombre = nombre;
+        this.telefono = telefono;
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCategoriaProductos() {
+        return categoriaProductos;
+    }
+
+    public void setCategoriaProductos(String categoriaProductos) {
+        this.categoriaProductos = categoriaProductos;
+    }
 
 }
