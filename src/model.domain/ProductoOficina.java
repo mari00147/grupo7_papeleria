3d0443b7cbcc4c171dfc7730c94b7e4d2919d764
Antonia Ruiz <antoniaruiz2403@gmail.com>
2026-09-06 18:11:35 -0500

    Actualiza productoOficina

--- src/model.domain/ProductoOficina.java
+++ src/model.domain/ProductoOficina.java
@@ -1,3 +1,22 @@
-public class ProductoOficina {
+public class ProductoOficina extends Producto {
+private String categoria;
 
+public ProductoOficina(String codigo, String nombre, double precio, int cantidadStock,String categoria) {
+    super(codigo,nombre, precio, cantidadStock);
+    this.categoria = categoria;
+
+}
+
+public String getCategoria() {
+    return categoria;
+}
+
+public void setCategoria(String categoria) {
+    this.categoria = categoria;
+}
+   
+
+public double CalcularPrecioFinal(){
+ return getPrecio()*1.08;
+}
 }
