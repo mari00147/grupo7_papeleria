75f890a6d53c45020f65a14c069a02a4adf4581f
Sebastian <s.sarrazola@visiontravelmail.net>
2026-09-26 16:43:50 -0500

    Crea un ItemVenta para cada producto y los agrega a una venta

--- src/PruebaCreacionObjetos.java
+++ src/PruebaCreacionObjetos.java
@@ -1,13 +1,37 @@
-import model.domain.Producto;
-import model.domain.ProductoEscolar;
 import model.domain.ProductoOficina;
+import model.domain.ProductoEscolar;
+import model.domain.ItemVenta;
+import model.domain.Venta;
 
 public class PruebaCreacionObjetos {
+
     public static void main(String[] args) {
-        ProductoOficina oficina = new ProductoOficina("001", "lapiz", 1500, 100, "escritura");
-        ProductoEscolar escolar = new ProductoEscolar("002", "Cuaderno", 5000, 100, "primaria");
+
+        ProductoOficina oficina = new ProductoOficina(
+                "001",
+                "lapiz",
+                1500,
+                100,
+                "escritura");
+
+        ProductoEscolar escolar = new ProductoEscolar(
+                "002",
+                "Cuaderno",
+                5000,
+                100,
+                "primaria");
 
         System.out.println("Precio final oficina: " + oficina.calcularPrecioFinal());
         System.out.println("Precio final escolar: " + escolar.calcularPrecioFinal());
+
+        ItemVenta itemOficina = new ItemVenta(oficina, 2);
+        ItemVenta itemEscolar = new ItemVenta(escolar, 3);
+
+        Venta venta = new Venta();
+
+        venta.agregarItem(itemOficina);
+        venta.agregarItem(itemEscolar);
+
+        System.out.println("Cantidad de items en la venta: " + venta.getItems().size());
     }
 }
